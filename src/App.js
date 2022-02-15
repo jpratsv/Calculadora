0a60e058f201f3b5f8e38e1244b687e757be3c2b
jpratsv <46995951+jpratsv@users.noreply.github.com>
2022-02-15 17:22:33 +0100

    15/02/2022

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import CaptaTecla from "./CaptaTecla";
+
+import DisplayCalculadora from "./Display";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DisplayCalculadora />
+    </div>
+  );
+}
